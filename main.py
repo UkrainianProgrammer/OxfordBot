b53182f0cc714646d1b43df32ccd34109d369dbc
Oleksandr Sofishchenko <insten490@gmail.com>
2017-06-18 18:33:57 -0700

    initial commit; rough outline

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+# OxfordBot is based on Oxford Dictionaries API
+
+"""
+ideas: 
+ - direct word search
+ - daily notifications
+ - practice common SAT/GRE words (optional for now) -- will requre a separate API
+"""
+
+print ("Initial commit, project begins soon")
